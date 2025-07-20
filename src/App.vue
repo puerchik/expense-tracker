32c942dc9f5f7dadb3325eed96426442846325e5
puerchik <rumyantsevnn@yandex.ru>
2025-07-20 07:41:51 +0300

    init: start project, start component

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Balance from './components/Balance.vue'
+import TitleBar from './components/TitleBar.vue'
+</script>
+
+<template>
+  <div class="container">
+    <title-bar></title-bar>
+    <balance></balance>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Balance.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1 id="balance">$0.00</h1>
+</template>
+
+<style scoped></style>
